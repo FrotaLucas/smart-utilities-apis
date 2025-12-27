8ce3f16890eb536bd5e303edd9f81589e5f86549
FrotaLucas <lucasdiasfrota@gmail.com>
2025-12-27 21:34:36 +0100

    new notations

--- src/main/java/project/demo/model/Reading.java
+++ src/main/java/project/demo/model/Reading.java
@@ -4,12 +4,16 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "readings")
 public class Reading {
     
     @Id
