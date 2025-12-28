926cddcad59c5e844e1de93b124babf5c42e8cba
FrotaLucas <lucasdiasfrota@gmail.com>
2025-12-28 21:08:27 +0100

    first method CRUD

--- src/main/java/project/demo/service/Customer/CustomerService.java
+++ src/main/java/project/demo/service/Customer/CustomerService.java
@@ -1,5 +1,19 @@
 package project.demo.service.Customer;
 
+import project.demo.model.Customer;
+import project.demo.repository.CustomerRepository.CustomerRepository;
+
 public class CustomerService {
-    
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
+    public Customer addNewCustomer (Customer customer) {
+        return customerRepository.save(customer);
+    }   
+
 }
