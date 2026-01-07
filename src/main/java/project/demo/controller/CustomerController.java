4a97e4342e28e758fb9d666b7290ddb5ba97d8c6
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-07 19:20:36 +0100

    finish post method

--- src/main/java/project/demo/controller/CustomerController.java
+++ src/main/java/project/demo/controller/CustomerController.java
@@ -1,8 +1,13 @@
 package project.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import project.demo.model.Customer;
 import project.demo.service.Customer.CustomerService;
 
 
@@ -15,4 +20,16 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer)
+    {
+        Customer createdCustomer = customerService.createCustomer(customer);
+        
+        //toDo
+        //handle exceptions and validation
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
+    }
+
 }
