fc1b145d2f7bd3b3d898022e673189c889cfa79e
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-14 18:01:53 +0100

    implement interface JPA

--- src/main/java/project/demo/repository/ReadingRepository/ReadingRepository.java
+++ src/main/java/project/demo/repository/ReadingRepository/ReadingRepository.java
@@ -1,5 +1,10 @@
 package project.demo.repository.ReadingRepository;
 
-public class ReadingRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import project.demo.model.Reading;
+
+public interface ReadingRepository extends JpaRepository<Reading, Long>
+{
     
 }
