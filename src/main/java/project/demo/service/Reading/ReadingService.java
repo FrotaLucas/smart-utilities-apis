7c9d6d6f069129f9aec3e06461a0b4327cf87941
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-22 17:28:24 +0100

    create new methods

--- src/main/java/project/demo/service/Reading/ReadingService.java
+++ src/main/java/project/demo/service/Reading/ReadingService.java
@@ -1,8 +1,18 @@
 package project.demo.service.Reading;
 
+import java.util.List;
+
 import project.demo.model.Reading;
 
 public interface ReadingService {
     
     Reading createReading(Reading reading);
+
+    Reading getReadingById (Reading reading);
+ 
+    List<Reading> getAllReadings();
+
+    void deleleteReadingById (Long id);
+
+    Reading updateReading (Long id, Reading reading);
 }
--- src/main/java/project/demo/service/Reading/ReadingServiceImpl.java
+++ src/main/java/project/demo/service/Reading/ReadingServiceImpl.java
@@ -1,5 +1,7 @@
 package project.demo.service.Reading;
 
+import java.util.List;
+
 import project.demo.model.Reading;
 import project.demo.repository.ReadingRepository.ReadingRepository;
 
@@ -20,7 +22,32 @@ public class ReadingServiceImpl implements ReadingService{
         return createdReading;
     }
 
-    
 
-    //implementations
+    @Override
+    public Reading getReadingById(Reading reading) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getReadingById'");
+    }
+
+
+    @Override
+    public List<Reading> getAllReadings() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllReadings'");
+    }
+
+
+    @Override
+    public void deleleteReadingById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleleteReadingById'");
+    }
+
+
+    @Override
+    public Reading updateReading(Long id, Reading reading) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateReading'");
+    }
+
 }
