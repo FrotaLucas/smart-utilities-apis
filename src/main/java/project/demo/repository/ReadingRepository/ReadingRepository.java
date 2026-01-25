851f07991bfb04f9f0db6430b983a377ae0c0155
FrotaLucas <lucasdiasfrota@gmail.com>
2026-01-25 21:13:02 +0100

    new method JPA

--- src/main/java/project/demo/repository/ReadingRepository/ReadingRepository.java
+++ src/main/java/project/demo/repository/ReadingRepository/ReadingRepository.java
@@ -1,10 +1,14 @@
 package project.demo.repository.ReadingRepository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import project.demo.model.Reading;
 
+//Spring Data JPA repository 
 public interface ReadingRepository extends JpaRepository<Reading, Long>
 {
+    List<Reading> findByCustomerId(Long customerId);
     
 }
